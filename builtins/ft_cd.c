e61551b3478cf8c3d644724f057e371eb10a17e9
tayqiter00 <tayqiter00@gmail.com>
2024-10-05 23:03:13 +0800

    Fixed valgrind throwing error for ft_cd():
    "Conditional jump or move depends on uninitialised value(s)"

--- builtins/ft_cd.c
+++ builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: qtay <qtay@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 16:48:20 by qtay              #+#    #+#             */
-/*   Updated: 2024/09/08 17:30:40 by qtay             ###   ########.fr       */
+/*   Updated: 2024/10/05 03:47:30 by qtay             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,11 @@ int	cd_home(char **envp)
 		if (!strncmp(*envp, "HOME=", 5)) // ft
 			break ;
 		*envp++;
+	}
+	if (*envp == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: HOME not found\n");
+		return (1);
 	}
 	return (chdir(*envp + 5));
 }
@@ -54,17 +59,27 @@ void	change_oldpwd(char *curdir, char **envp)
 void	change_pwd(char **envp)
 {
 	char	s[PATH_MAX];
+	char	*cwd;
 
 	while (*envp)
 	{
-		if (!strncmp(*envp, "PWD=", 4)) // ft
+		if (!ft_strncmp(*envp, "PWD=", 4)) // ft
 		{
+			cwd = getcwd(s, PATH_MAX - 1);
+			if (!cwd)
+			{
+				dprintf(STDERR_FILENO, "getcwd failed\n");
+				return ;
+			}
 			free(*envp);
-			*envp = getcwd(s, PATH_MAX - 1);
+			*envp = ft_strdup(cwd);
+			if (!*envp)
+   				dprintf(STDERR_FILENO, "ft_strdup failed\n");
 			break ;
 		}
 		*envp++;
 	}
+
 }
 
 /**
@@ -83,6 +98,11 @@ int	ft_cd(t_tokennode *args, char **envp)
 
 	errno = 0;
 	curdir = getenv("PWD");
+	if (curdir == NULL) // leave it first
+	{
+		dprintf(STDERR_FILENO, "Error: PWD environment variable not found\n");
+		return (1);
+	}
 	if (!args || args->is_meta_char)
 		errno = cd_home(envp);
 	else if (args->next == NULL)
