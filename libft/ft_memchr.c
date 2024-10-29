fb1ecb7040b29f4e1a0191ab6136d803a181a0bd
tayqiter00 <tayqiter00@gmail.com>
2024-10-29 14:47:57 +0800

    update ft_memchr() to silence warning

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qtay <qtay@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: qtay <qtay@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 11:30:55 by qtay              #+#    #+#             */
-/*   Updated: 2024/02/29 16:42:56 by qtay             ###   ########.fr       */
+/*   Updated: 2024/10/29 14:27:37 by qtay             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,19 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	unsigned char	*pt_s;
-	unsigned char	uc;
-	size_t			i;
+	const unsigned char	*pt_s;
+	unsigned char		uc;
+	size_t				i;
 
-	pt_s = (unsigned char *)s;
+	pt_s = (const unsigned char *)s;
 	uc = (unsigned char)c;
 	i = 0;
 	while (i < n)
 	{
 		if (pt_s[i] == uc)
-			return ((void *)&s[i]);
+			return ((void *)(pt_s + i));
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
+
